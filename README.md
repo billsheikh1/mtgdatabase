a915cacbdf18aec456fa90f298e0fee9c47b7fbb
Billie Sheikh <bill.sheikh@bbc.co.uk>
2023-10-08 22:47:13 +0100

    Revert "refactor"
    This reverts commit bd2486ddbb1a235b4ae41270de17b81904305e39.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # mtgdatabase
-
-Username: mtgadmin
-Password: AUuEijsyLRn2iVTM
-mongodb+srv://mtgadmin:AUuEijsyLRn2iVTM@mtgdatabase.amvkuy9.mongodb.net/?retryWrites=true&w=majority
---
+++ app/src/service.js
@@ -0,0 +1,15 @@
+const scryfallApi = 'https://api.scryfall.com/cards/'
+const localApi = 'http://localhost:1000'
+const autocomplete = `${scryfallApi}autocomplete?q=`
+
+export const getAutocomplete = (str) => {
+  return fetch(`${autocomplete}${str}`).then((res) => res.json())
+}
+
+export const postCard = (card) => {
+  return fetch(`${localApi}/addCard?name=${card}`)
+}
+
+export const getCards = () => {
+  return fetch(`${localApi}/cards`).then((res) => res.json())
+}
